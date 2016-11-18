88545ab07dc5452d327cce6e5821667ec528dd5f
Umair Abbas Hayat <umair.abbas.hayat@ericsson.com>
2016-11-18 01:25:34 +0100

    -OOSCDateTime class completed -Some errors occour in the OOSCDate class, which hinters the functionality!

--- Assignement1/src/date/App.java
+++ Assignement1/src/date/App.java
@@ -3,8 +3,28 @@ package date;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			//testing
+		
+//		OOSCDateTime d = new OOSCDateTime();
+//		//d.setDate(2004, 3, 2);
+//		d.setTime(5, 6, 7);
+//		d.addHours(50);
+//		System.out.print(d.getTime());
+	
+//		OOSCDate d = new OOSCDate();
+//		d.addDays(2);
+		
+		try {
+			OOSCDateTime d = new OOSCDateTime();
+			System.out.print(d.toString()+"\t \t"+d.getTime()+"\n");
+			
+			d.setDate(2004, 11, 2);
+			d.setTime(05, 6, 7);
+			System.out.print(d.toString()+"\t \t"+d.getTime()+"\n");
+		} catch (AssertionError e) {
+			System.out.print(e.toString());
+		    System.exit(0);
+		}  
+		
 	}
 
 }
