9fa9facb7095742b74f60baa703ac4c61bc3b119
Tarek <tarek.hassan.it@gmail.com>
2016-11-19 15:08:00 +0100

    More tests until the method (add Minutes)! Please fix Origional code! many failures

--- Assignement1/test/date/OOSCDateTimeTest.java
+++ Assignement1/test/date/OOSCDateTimeTest.java
@@ -73,19 +73,165 @@ public class OOSCDateTimeTest {
 		test.setTime(0,0,0);
 		assertEquals("00:00:00", test.getTime());
 	}
-	
-	
+	@Test
+	public void setDateTimeTestYear(){
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(-1, 0, 0, 0, 0, 0);
+		test.setTime(0,0,0);		
+	}
+	@Test
+	public void setDateTimeTestMonth(){
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(0, -1, 0, 0, 0, 0)	;
+	}
+	@Test
+	public void setDateTimeTestDay(){
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(0, 0, -1, 0, 0, 0);		
+	}
+	@Test(expected = java.lang.Error.class)
+	public void setDateTimeTestHour(){
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(0, 0, 0, -1, 0, 0);		
+	}
+	@Test(expected = java.lang.Error.class)
+	public void setDateTimeTestMinutes(){
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(0, 0, 0, 0, -1, 0);		
+	}
+	@Test(expected = java.lang.Error.class)
+	public void setDateTimeTestSeconds(){
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(0, 0, 0, 0, 0, -1);		
+	}
 	
 	@Test(expected = java.lang.Error.class)
-	public void setHours() {
+	public void setHoursLowerBound() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setHours(-1);
+		
+	}
+	@Test(expected = java.lang.Error.class)
+	public void setHoursUpperBound() {
 		OOSCDateTime test = new OOSCDateTime();
 		test.setHours(24);
 		
+	}
+	@Test
+	public void setHoursTest() {
+		OOSCDateTime test = new OOSCDateTime();
+		for (int i = 0; i < 24; i++)
+		test.setHours(i);
+		
+	}
+	@Test(expected = java.lang.Error.class)
+	public void setMinutesLowerBound() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setMinutes(-1);
+		
+	}
+	@Test(expected = java.lang.Error.class)
+	public void setMinutesUpperBound() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setMinutes(60);
+		
+	}
+	@Test
+	public void setMinutesTest() {
+		OOSCDateTime test = new OOSCDateTime();
+		for (int i = 0; i < 60; i++)
+		test.setMinutes(i);
 		
 	}
 	
-
+	@Test(expected = java.lang.Error.class)
+	public void setSecondsLowerBound() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setSeconds(-1);
+		
+	}
+	@Test(expected = java.lang.Error.class)
+	public void setSecondsUpperBound() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setSeconds(60);
+		
+	}
+	@Test
+	public void setSecondsTest() {
+		OOSCDateTime test = new OOSCDateTime();
+		for (int i = 0; i < 60; i++)
+		test.setSeconds(i);
+		
+	}
 	
+	@Test
+	public void getHoursTest() {
+		OOSCDateTime test = new OOSCDateTime();
+		for (int i = 0; i < 24; i++){
+			test.setHours(i);
+			assertEquals(i, test.getHours());
+		}
+	}
+	
+
+	@Test
+	public void getMinutesTest() {
+		OOSCDateTime test = new OOSCDateTime();
+		for (int i = 0; i < 60; i++){
+			test.setMinutes(i);
+			assertEquals(i, test.getMinutes());
+		}
+	}
+	@Test
+	public void getSecondsTest() {
+		OOSCDateTime test = new OOSCDateTime();
+		for (int i = 0; i < 60; i++){
+			test.setSeconds(i);
+			assertEquals(i, test.getSeconds());
+		}
+	}
+	
+	@Test(expected = java.lang.Error.class)
+	public void addHoursZero() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.addHours(0);
+	}
+	
+	@Test
+	public void addHoursTest() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setTime(0, 0, 0);
+		test.addHours(2);
+		assertEquals(2, test.getHours());
+	}
+
+	@Test
+	public void addHoursNewDay() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(1, 1, 1, 23, 0, 0);
+		test.addHours(1);
+		assertEquals(0, test.getHours());
+		assertEquals(2, test.getDay());
+	}
+	@Test
+	public void addHoursNewMonth() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(1, 1, 31, 23, 0, 0);
+		test.addHours(1);
+		assertEquals(0, test.getHours());
+		assertEquals(1, test.getDay());
+		assertEquals(2, test.getMonth());
+	}
+	@Test
+	public void addHoursNewYear() {
+		OOSCDateTime test = new OOSCDateTime();
+		test.setDateTime(1, 12, 31, 23, 0, 0);
+		test.addHours(1);
+		assertEquals(0, test.getHours());
+		assertEquals(1, test.getDay());
+		assertEquals(1, test.getMonth());
+		assertEquals(2, test.getYear());
+	}
 
 	@Test(expected = java.lang.Error.class)
 	public void addMinutesZero() {
@@ -93,15 +239,15 @@ public class OOSCDateTimeTest {
 		test.addMinutes(0);
 	}
 	
-/*	@Test
+	@Test
 	public void addMinutesTest() {
 		OOSCDateTime test = new OOSCDateTime();
-		test.setTime(4, 0, 0);
+		test.setTime(0, 0, 0);
 		test.addMinutes(1);
-		assertEquals(4, test.getHours());
+		assertEquals(0, test.getHours());
 		assertEquals(1, test.getMinutes());
 		assertEquals(0, test.getSeconds());
-	}*/
+	}
 	
 	public void addMinutesTestNewHour() {
 		OOSCDateTime test = new OOSCDateTime();
